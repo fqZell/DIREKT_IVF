1b38c762286c23ed6c63bfacccce9fe40e6f4bbc
fqZell <mulkyway00@gmail.com>
2024-10-11 18:08:27 +0300

    add to menu banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,23 +55,84 @@ sliderBanner = () => {
 burgerBanner = () => {
     const headerBurger = document.querySelector('.header-burger');
     const headerMenu = document.querySelector('.burger-menu');
+    const infoContent = document.querySelector('.info-layout');
+    const headerEn = document.querySelector('.header-en');
+    const headerLine = document.querySelectorAll('.header-line');
+    const logo = document.querySelector('.header-logo img');
+    const searchIcon = document.querySelector('.header-search img');
+    const headerSearch = document.querySelector('.search-menu');
 
+
+    if (!headerBurger) return;
     headerBurger.addEventListener('click', () => {
 
-        document.body.style.overflow = 'hidden';
-        headerMenu.classList.toggle('active');
+        if (headerSearch.classList.contains('active')) {
+            document.body.classList.remove('active');
+            headerSearch.classList.remove('active');
+            infoContent.classList.remove('active');
+            headerEn.classList.remove('active');
+            headerBurger.classList.remove('active');
+            headerLine.forEach(line => {
+                line.classList.remove('active'); 
+            });
+    
+            logo.classList.remove('active');
+            searchIcon.classList.remove('active');
+        }
 
-    })
-}
+        document.body.classList.toggle('active');
+        headerMenu.classList.toggle('active');
+        infoContent.classList.toggle('active');
+        headerEn.classList.toggle('active');
+        headerBurger.classList.toggle('active');
+        headerLine.forEach(line => {
+            line.classList.toggle('active'); 
+        });
+
+        logo.classList.toggle('active');
+        searchIcon.classList.toggle('active');
+    });
+};
 
 searchBanner = () => {
     const searchBurger = document.querySelector('.header-search');
+    const headerBurger = document.querySelector('.header-burger');
     const searchMenu = document.querySelector('.search-menu');
+    const infoContent = document.querySelector('.info-layout');
+    const headerEn = document.querySelector('.header-en');
+    const headerLine = document.querySelectorAll('.header-line');
+    const logo = document.querySelector('.header-logo img');
+    const searchIcon = document.querySelector('.header-search img');
+    const headerMenu = document.querySelector('.burger-menu');
 
+    if (!searchBurger) return;
     searchBurger.addEventListener('click', () => {
 
-        document.body.style.overflow = 'hidden';
+        if (headerMenu.classList.contains('active')) {
+            document.body.classList.remove('active');
+            headerMenu.classList.remove('active');
+            infoContent.classList.remove('active');
+            headerEn.classList.remove('active');
+            headerBurger.classList.remove('active');
+            headerLine.forEach(line => {
+                line.classList.remove('active'); 
+            });
+    
+            logo.classList.remove('active');
+            searchIcon.classList.remove('active');
+        }
+
+        document.body.classList.toggle('active');
         searchMenu.classList.toggle('active');
+        infoContent.classList.toggle('active');
+        headerEn.classList.toggle('active');
+        headerBurger.classList.toggle('active');
+        headerLine.forEach(line => {
+            line.classList.toggle('active'); 
+        });
+
+        logo.classList.toggle('active');
+        searchIcon.classList.toggle('active');
 
     })
 }
