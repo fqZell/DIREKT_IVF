37c80278f20594684b83c6e18937e52b4e7bf93f
fqZell <mulkyway00@gmail.com>
2024-12-04 14:55:29 +0300

    slider arrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,14 @@ sliderBanner = () => {
     function updateSlider() {
         const offset = -currentSlide * 100; 
         sliderLayout.style.transform = `translateX(${offset}%)`;
-
+    
         banners.forEach((banner, index) => {
             const bannerContent = banner.querySelector('.banner-content');
             const img = banner.querySelector('img');
-
+    
             bannerContent.classList.remove('active');
             img.classList.remove('active');
-
+    
             if (index === currentSlide) {
                 setTimeout(() => {
                     bannerContent.classList.add('active');
@@ -27,17 +27,29 @@ sliderBanner = () => {
                 }, 2000);
             }
         });
-
+    
         const breakpoints = document.querySelectorAll('.banner-breakpoint');
         breakpoints.forEach((bp, index) => {
             bp.classList.toggle('active', index === currentSlide);
         });
-
+    
         slideNumber.textContent = (currentSlide + 1).toString().padStart(2, '0');
-
+    
         rotationDegree += 360;
         logo.style.transform = `rotate(${rotationDegree}deg)`; 
+    
+        // Проверяем разрешение экрана
+        if (window.innerWidth < 769) {
+            prevButton.style.opacity = '1'; // Всегда показывать на маленьких экранах
+        } else if (currentSlide > 0) {
+            prevButton.style.opacity = '1'; // Показывает на всех слайдах, кроме первого
+        } else {
+            prevButton.style.opacity = '0'; // Скрывает на первом слайде
+        }
     }
+
+    window.addEventListener('resize', updateSlider);
+        
 
     prevButton.addEventListener('click', () => {
         currentSlide = (currentSlide === 0) ? banners.length - 1 : currentSlide - 1;
