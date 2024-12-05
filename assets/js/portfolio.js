75b03e1d0c74f28324808005fc905cf1e364eb6a
fqZell <mulkyway00@gmail.com>
2024-12-05 19:00:23 +0300

    05.12.24

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,9 +4,7 @@ const init = () => {
     changeLanguage()
     fadeInSection()
     burgerFadeIn()
-    scrollHeader()
     tebsHonors()
-    popup()
 }
 
 document.addEventListener('DOMContentLoaded', init);
@@ -15,24 +13,22 @@ document.addEventListener('DOMContentLoaded', init);
 burgerBanner = () => {
     const headerBurger = document.querySelector('.header-burger');
     const headerMenu = document.querySelector('.burger-menu');
+    // const infoContent = document.querySelector('.info-layout');
     const headerEn = document.querySelector('.header-en');
     const headerLine = document.querySelectorAll('.header-line');
     const logo = document.querySelector('.header-logo img');
     const logoTxt = document.querySelector('.header-logo__txt')
     const searchIcon = document.querySelector('.header-search img');
     const headerSearch = document.querySelector('.search-menu');
-    const bannerHeight = window.innerHeight;
     const htmlTag = document.querySelector('html')
 
     if (!headerBurger) return;
     headerBurger.addEventListener('click', () => {
 
-        const scrollPosition = window.scrollY;
-        const isInBanner = scrollPosition <= bannerHeight;
-
         if (headerSearch.classList.contains('active')) {
             document.body.classList.remove('active');
             headerSearch.classList.remove('active');
+            // infoContent.classList.remove('active');
             headerEn.classList.remove('active');
             headerBurger.classList.remove('active');
             headerLine.forEach(line => {
@@ -45,6 +41,7 @@ burgerBanner = () => {
 
         document.body.classList.toggle('active');
         headerMenu.classList.toggle('active');
+        // infoContent.classList.toggle('active');
         headerEn.classList.toggle('active');
         headerBurger.classList.toggle('active');
         htmlTag.classList.toggle('active')
@@ -53,19 +50,21 @@ burgerBanner = () => {
         });
 
         if (document.body.classList.contains('active')) {
+            logo.src = '../assets/img/logo/logoBlue.svg'; 
+        } else {
             logo.src = '../assets/img/logo/logoBlue.svg';
+        }
+
+        if (document.body.classList.contains('active')) {
+            logoTxt.src = '../assets/img/logo/logoTxtBlue.svg'; 
+        } else {
             logoTxt.src = '../assets/img/logo/logoTxtBlue.svg';
+        }
+
+        if (document.body.classList.contains('active')) {
+            searchIcon.src = '../assets/img/icons/searchBlue.svg'; 
+        } else {
             searchIcon.src = '../assets/img/icons/searchBlue.svg';
-        } else if (!isInBanner) {
-            // Если находимся вне секции баннера, возвращаем изображения в "внешний" вид
-            logo.src = '../assets/img/logo/logoBlue.svg';
-            logoTxt.src = '../assets/img/logo/logoTxtBlue.svg';
-            searchIcon.src = '../assets/img/icons/searchBlue.svg';
-        } else if(isInBanner) {
-            // Если внутри секции баннера, возвращаем изображения в "баннерный" вид
-            logo.src = '../assets/img/logo/logo.svg';
-            logoTxt.src = '../assets/img/logo/logoTxt.svg';
-            searchIcon.src = '../assets/img/icons/search.svg';
         }
 
         searchIcon.classList.toggle('active');
@@ -75,14 +74,13 @@ burgerBanner = () => {
 searchBanner = () => {
     const searchBurger = document.querySelector('.header-search');  
     const headerBurger = document.querySelector('.header-burger');  
-    const searchMenu = document.querySelector('.search-menu');  
+    const searchMenu = document.querySelector('.search-menu');   
     const headerEn = document.querySelector('.header-en');  
     const headerLine = document.querySelectorAll('.header-line');  
     const logo = document.querySelector('.header-logo img');  
     const logoTxt = document.querySelector('.header-logo__txt')
     const searchIcon = document.querySelector('.header-search img');  
     const headerMenu = document.querySelector('.burger-menu');  
-    const bannerHeight = window.innerHeight;
     const htmlTag = document.querySelector('html')
    
     if (!searchBurger) return;  
@@ -90,13 +88,9 @@ searchBanner = () => {
     headerBurger.style.display = "flex";  
    
     searchBurger.addEventListener('click', () => {  
-
-        const scrollPosition = window.scrollY;
-        const isInBanner = scrollPosition <= bannerHeight;
-
     if (headerMenu.classList.contains('active')) {  
     document.body.classList.remove('active');  
-    headerMenu.classList.remove('active');   
+    headerMenu.classList.remove('active');  
     headerEn.classList.remove('active');  
     headerBurger.classList.remove('active');  
     headerLine.forEach(line => {  
@@ -107,8 +101,9 @@ searchBanner = () => {
     }  
    
     document.body.classList.toggle('active');  
-    searchMenu.classList.toggle('active');    
-    headerEn.classList.toggle('active');  
+    searchMenu.classList.toggle('active');   
+    headerEn.classList.toggle('active'); 
+    htmlTag.classList.toggle('active') 
    
     headerBurger.style.display = (headerBurger.style.display === "none" || headerBurger.style.display === "") ? "flex" : "none";  
    
@@ -117,22 +112,23 @@ searchBanner = () => {
    
     logo.classList.toggle('active');  
     searchIcon.classList.toggle('active');  
-    htmlTag.classList.toggle('active')
 
     if (document.body.classList.contains('active')) {
-        logo.src = '../assets/img/logo/logoBlue.svg';
-        logoTxt.src = '../assets/img/logo/logoTxtBlue.svg';
-        searchIcon.src = '../assets/img/icons/cross.svg';
-    } else if (!isInBanner) {
-        // Если находимся вне секции баннера, возвращаем изображения в "внешний" вид
-        logo.src = '../assets/img/logo/logoBlue.svg';
-        logoTxt.src = '../assets/img/logo/logoTxtBlue.svg';
-        searchIcon.src = '../assets/img/icons/searchBlue.svg';
+        logo.src = '../assets/img/logo/logoBlue.svg'; 
     } else {
-        // Если внутри секции баннера, возвращаем изображения в "баннерный" вид
-        logo.src = '../assets/img/logo/logo.svg';
-        logoTxt.src = '../assets/img/logo/logoTxt.svg';
-        searchIcon.src = '../assets/img/icons/search.svg';
+        logo.src = '../assets/img/logo/logoBlue.svg';
+    }
+
+    if (document.body.classList.contains('active')) {
+        logoTxt.src = '../assets/img/logo/logoTxtBlue.svg'; 
+    } else {
+        logoTxt.src = '../assets/img/logo/logoTxtBlue.svg';
+    }
+
+    if (document.body.classList.contains('active')) {
+        searchIcon.src = '../assets/img/icons/cross.svg'; 
+    } else {
+        searchIcon.src = '../assets/img/icons/searchBlue.svg';
     }
 
     });  
@@ -215,44 +211,8 @@ const burgerFadeIn = () => {
     });
 };
 
-function scrollHeader() {
-    const headerLogo = document.querySelector('.header-logo img');
-    const headerLogoTxt = document.querySelector('.header-logo__txt');
-    const searchIcon = document.querySelector('.header-search img');
-    const headerEn = document.querySelector('.header-en');
-    const headerLines = document.querySelectorAll('.header-line');
-    const headerWrapper = document.querySelector(".header-wrapper")
-    
-    const bannerHeight = window.innerHeight; 
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > bannerHeight) {
-        headerLogo.src = '../assets/img/logo/logoBlue.svg'; 
-        headerLogoTxt.src = '../assets/img/logo/logoTxtBlue.svg';
-        searchIcon.src = '../assets/img/icons/searchBlue.svg';
-        headerEn.style.color = "#ffffff";
-        headerEn.style.backgroundColor = "#00328A";
-        headerLines.forEach(headerLine => {
-            headerLine.style.backgroundColor = "#00328A";
-        });
-        headerWrapper.classList.add('active')
-    } else {
-        headerLogo.src = '../assets/img/logo/logo.svg'; 
-        headerLogoTxt.src = '../assets/img/logo/logoTxt.svg';
-        searchIcon.src = '../assets/img/icons/search.svg';
-        headerEn.style.color = "#000";
-        headerEn.style.backgroundColor = "#fff";
-        headerLines.forEach(headerLine => {
-            headerLine.style.backgroundColor = "#fff";
-        });
-        headerWrapper.classList.remove('active')
-    }
-}
-
-window.addEventListener('scroll', scrollHeader);
-
 const tebsHonors = () => {
-    const honorsWrapper = document.querySelectorAll('.honors-wrapper');
+    const honorsWrapper = document.querySelectorAll('.portfolio-wrapper');
     if (!honorsWrapper.length) return;
     const honorsNumbers = document.querySelectorAll('.honors-bottom__numbers span');
     if (!honorsNumbers.length) return;
@@ -295,38 +255,3 @@ const tebsHonors = () => {
     fullPrevButton.addEventListener('click', () => updateActiveSlide(0)); // В начало
     fullNextButton.addEventListener('click', () => updateActiveSlide(honorsWrapper.length - 1)); // В конец
 };
-
-const popup = () => {
-
-    const cards = document.querySelectorAll('.honors-wrapper__card');
-    const popupOverlay = document.querySelector('.popup-overlay');
-    const popupImage = document.querySelector('.popup-image');
-    const popupTitle = document.querySelector('.popup-title');
-
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            const imgSrc = card.querySelector('img').src;
-            const titleText = card.querySelector('.honors-card__title').innerText;
-
-            // Установка картинки и заголовка в popup
-            popupImage.src = imgSrc;
-            popupTitle.innerText = titleText;
-
-            // Показ popup
-            popupOverlay.style.display = 'flex';
-        });
-    });
-
-    // Закрытие popup по клику на overlay
-    popupOverlay.addEventListener('click', (e) => {
-        if (e.target === popupOverlay) {
-            popupOverlay.classList.add('hidden');
-            setTimeout(() => {
-                popupOverlay.style.display = 'none';
-                popupOverlay.classList.remove('hidden');
-            }, 500);
-        }
-    });
-
-
-}
